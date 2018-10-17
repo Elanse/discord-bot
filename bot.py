cbc5d5cf7fcaa5a8b6c5359ef92c0268e9b34fcc
Elanse <33039002+Elanse@users.noreply.github.com>
2018-10-17 14:20:18 -0700

    start up bot

--- bot.py
+++ bot.py
@@ -14,3 +14,9 @@ async def on_ready():
     print(client.user)
 
 @client.event
+async def on_message(message):
+    if message.author != client.user:
+        await  client.send_message(message.channel, message.content[::-1])
+
+token = os.environ.get(token)
+client.run(token)
