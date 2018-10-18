5ceec0a425897270659491a765b243460d3bbd60
Elanse <33039002+Elanse@users.noreply.github.com>
2018-10-18 11:49:50 -0700

    adjust code to get to run

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 # import discord.py
-import discord.io
+import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
 import os
 
 # set a discord client
-client = discord.Client()
+Client = discord.Client()
+client = command.Bot(command_prefix = "?")
 
 @client.event
 async def on_ready():
@@ -18,5 +19,4 @@ async def on_message(message):
     if message.author != client.user:
         await client.send_message(message.channel, message.content[::-1])
 
-token = os.environ.get( "NTAxNTIxODYwODI0MDA2NjY4.DqapaA.su1bMmowu0Q6Atiav19OmmSgflc")
-client.run(token)
+client.run("NTAxNTIxODYwODI0MDA2NjY4.DqapaA.su1bMmowu0Q6Atiav19OmmSgflc")
