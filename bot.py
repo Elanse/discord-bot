f58ee465a55251c6633b3a21397e1f3c0e53634c
Elanse <33039002+Elanse@users.noreply.github.com>
2018-10-18 13:38:51 -0700

    find sourcecode on google and c/p it in

--- bot.py
+++ bot.py
@@ -1,13 +1,40 @@
 # import discord.py
+import dbl
 import discord
-from discord.ext.commands import Bot
 from discord.ext import commands
-import asyncio
-import os
 
+import aiohttp
+import asyncio
+import logging
+
+class DiscordBotsOrgAPI:
+    """Handles interactions with the discordbots.org API"""
+
+
+def __init__(self, bot):
+    self.bot = bot
+    self.token = 'dbl_token'  #  set this to your DBL token
+    self.dblpy = dbl.Client(self.bot, self.token)
+    self.bot.loop.create_task(self.update_stats())
+
+async def update_stats(self):
+    """This function runs every 30 minutes to automatically update your server count"""
+
+    while True:
+        logger.info('attempting to post server count')
+        try:
+            await self.dblpy.post_server_count()
+            logger.info('posted server count ({})'.format(len(self.bot.guilds)))
+        except Exception as e:
+            logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
+        await asyncio.sleep(1800)
+
+
+def setup(bot):
+    global logger
+    logger = logging.getLogger('bot')
+    bot.add_cog(DiscordBotsOrgAPI(bot))
 # set a discord client
-Client = discord.Client()
-client = command.Bot(command_prefix = "?")
 
 @client.event
 async def on_ready():
