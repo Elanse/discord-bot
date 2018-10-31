34931e99b1da54552bf570c3441a86271be31780
Elanse <33039002+Elanse@users.noreply.github.com>
2018-10-31 14:41:20 -0700

    get bot online

--- bot.py
+++ bot.py
@@ -1,36 +1,21 @@
-import dbl
 import discord
+from discord.ext.commands import Bot
 from discord.ext import commands
-
-import aiohttp
 import asyncio
-import logging
+import random
+from discord import Game
 
+Client = discord.client
+client = commands.Bot(command_prefix = "?")
 
-class DiscordBotsOrgAPI:
-    """Handles interactions with the discordbots.org API"""
+@client.event
+async def on_ready():
+    await client.change_presence(game = Game(name = "I'm alive!")))
+    print("Ready!")
 
-    def __init__(self, bot):
-        self.bot = bot
-        self.token = 'NTAxNTIxODYwODI0MDA2NjY4.DqapaA.su1bMmowu0Q6Atiav19OmmSgflc'
-  #  set this to your DBL token
-        self.dblpy = dbl.Client(self.bot, self.token)
-        self.bot.loop.create_task(self.update_stats())
-
-    async def update_stats(self):
-        """This function runs every 30 minutes to automatically update your server count"""
-
-        while True:
-            logger.info('attempting to post server count')
-            try:
-                await self.dblpy.post_server_count()
-                logger.info('posted server count ({})'.format(len(self.bot.guilds)))
-            except Exception as e:
-                logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
-            await asyncio.sleep(1800)
-
-
-def setup(bot):
-    global logger
-    logger = logging.getLogger('bot')
-    bot.add_cog(DiscordBotsOrgAPI(bot))
+@client.event
+async def on_message(message):
+    if message.content.upper() == "RAINDROP":
+        await client.send_message(message.channel, "DROPTOP")
+    if message.content.upper().startswith("HELLO"):
+        await client.send_message(message.channel, "Hi there!")
