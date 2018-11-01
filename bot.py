07c971281854c9405306d14a63a878291e19cac6
Elanse <33039002+Elanse@users.noreply.github.com>
2018-11-01 13:38:42 -0700

    watch tutorial to copy code

--- bot.py
+++ bot.py
@@ -3,15 +3,17 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
 import random
+import time
 from discord import Game
 
 Client = discord.client
 client = commands.Bot(command_prefix = "?")
+Clientdiscord = discord.Client()
 
 @client.event
 async def on_ready():
-    await client.change_presence(game = Game(name = "I'm alive!"))
-    print("Ready!")
+    await client.change_presence(game = Game(name = "   "))
+    print("Ready, Freddy!")
 
 @client.event
 async def on_message(message):
